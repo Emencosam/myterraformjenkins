12f0ce189d5c4b0cf50bc4aa168cdd96cd6a1f0f
Emencosam <emenco04@gmail.com>
2023-03-22 19:56:45 -0500

    added three additional servers

--- main.tf
+++ main.tf
@@ -6,3 +6,30 @@ resource "aws_instance" "web" {
     Name = "jenkins-pipedemo"
   }
 } 
+
+resource "aws_instance" "web-1" {
+  ami           = "ami-02f3f602d23f1659d"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "charis"
+  }
+} 
+
+resource "aws_instance" "web-2" {
+  ami           = "ami-02f3f602d23f1659d"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "peniel"
+  }
+} 
+
+resource "aws_instance" "web-3" {
+  ami           = "ami-02f3f602d23f1659d"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "perez"
+  }
+} 
